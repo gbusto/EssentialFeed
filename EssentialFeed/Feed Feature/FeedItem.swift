4f2eaf8dd43a31b88b637965f31164bbc9ef58d2
Gabe Busto <gbusto.gator@gmail.com>
2023-02-06 12:26:08 -0500

    Move Decodable logic to a new private Item struct
    This helps decouple the Feed Feature module from API implementation
    details

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -20,17 +20,3 @@ public struct FeedItem: Equatable {
         self.imageURL = imageURL
     }
 }
-
-extension FeedItem: Decodable {
-    // Provide mapping instructions because FeedItem has imageURL,
-    // but the JSON key needs to be "image"
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        // Red flag! This line below is an API detail that leaked into our feature module.
-        // This creates a tight coupling to the RemoteFeedLoader and is a detail of the RemoteFeedLoader that may not also apply to the LocalFeedLoader for example.
-        // This harmless string in the wrong module can end up breaking our abstractions :(
-        case imageURL = "image"
-    }
-}
