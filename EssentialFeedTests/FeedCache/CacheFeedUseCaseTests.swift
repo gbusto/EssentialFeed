311a354b8e5a78ef563fa6ca4c8ed0f5b6fc6590
Gabe Busto <gbusto.gator@gmail.com>
2023-03-04 16:15:23 -0500

    Removed unused code in test case

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -151,7 +151,6 @@ class CacheFeedUseCaseTests: XCTestCase {
     func test_save_succeedsOnSuccessfulCacheInsertion() {
         let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
-        let insertionError = anyNSError()
         let exp = expectation(description: "Wait for save completion")
         
         var receivedError: Error?
